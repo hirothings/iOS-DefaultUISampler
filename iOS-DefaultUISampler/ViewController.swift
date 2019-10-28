df1466e5b9aceceebd93ae5c2b2f77b922069eb9
hirothings <hiroshings.job1987@gmail.com>
2019-10-28 13:19:52 +0900

    Initial Commit

---
+++ iOS-DefaultUISampler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-DefaultUISampler
+//
+//  Created by sakamoto.hiroshi on 2019/10/28.
+//  Copyright © 2019 hirothings. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
